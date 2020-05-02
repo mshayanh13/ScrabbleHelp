d968264d7fa54b46b0d953905c643fafe1fe1eb7
Mohammad Shayan <mohammadhemani@gmail.com>
2020-05-02 15:02:24 +0430

    Initial Commit

---
+++ ScrabbleHelp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrabbleHelp
+//
+//  Created by Mohammad Shayan on 5/2/20.
+//  Copyright © 2020 Mohammad Shayan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
